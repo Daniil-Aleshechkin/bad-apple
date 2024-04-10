909fbec1a28eb929fd41c35d534d04f0f9c91ad3
Daniil Aleshechkin <aleshechkin.daniil@gmail.com>
2024-04-09 20:57:00 -0600

    Added the bad-apple files (bad practice I know, but I'm on a deadline)

--- terminal/terminal.py
+++ terminal/terminal.py
@@ -1,81 +1,26 @@
-import os
-import sys
-import tkinter as tk
-import serial
+from PIL import Image
+import numpy as np
 
-port = sys.argv[1]
-baudrate = 1500000
-ser = serial.Serial(port, baudrate, timeout=1)
+# Open the image file
+image_path = './bad-apple/bad_apple_1631.png'
+image = Image.open(image_path)
 
-MOVE_LEFT_CODE = b'\x34'
-MOVE_RIGHT_CODE = b'\x36'
-ROTATE_180_CODE = b'\x71'
-ROTATE_90_CODE = b'\x38'
-ROTATE_270_CODE = b'\x77'
-SOFT_DROP_CODE = b'\x35'
-HARD_DROP_CODE = b'\x64'
-HOLD_CODE = b'\x09'
-RESET_CODE = b'\x72'
-QUIT_CODE = b'\x70'
+# Resize the image
+image = image.crop((180, 0, image.width - 180, image.height))
+image = image.resize((32, 32))
+image = image.convert('L')
 
-def get_key_byte(event: tk.Event):
-    if event.keysym == "Left": # ARROW LEFT
-        return MOVE_LEFT_CODE
-    elif event.keysym == "Right": # ARROW RIGHT
-        return MOVE_RIGHT_CODE
-    elif event.keysym == "Up":
-        return ROTATE_90_CODE
-    elif event.keysym == "d":
-        return HARD_DROP_CODE
-    elif event.keysym == "r":
-        return RESET_CODE
-    elif event.keysym == "w":
-        return ROTATE_270_CODE
-    elif event.keysym == "q":
-        return ROTATE_180_CODE
-    elif event.keysym == "Tab":
-        return HOLD_CODE
-    elif event.keysym == "Down":
-        return SOFT_DROP_CODE
-    elif event.keysym == "p":
-        return QUIT_CODE
-    return 0x00
+# Define ASCII characters for different brightness levels
+black_char = "."
+white_char = "@"
 
-def on_key_press(event: tk.Event):
-    key = get_key_byte(event)
-    print("SENDING: ", key)
-    ser.write(key)
-    
-def on_key_released(event):
-    
-    key = get_key_byte(event)
-    
-    code = int.from_bytes(key, 'little')
-    code += 128
+# Convert image pixels to ASCII
+ascii_str = ''
+for i in range(image.size[1]):  # for every row
+    for j in range(image.size[0]):  # for every column
+        # Scale pixel value to 0-9
+        ascii_str += white_char if image.getpixel((j,i)) > 124 else black_char
+    ascii_str += '\n'
 
-    print("SENDING RELEASE: ", key)
-    ser.write(code.to_bytes(1, 'little'))
-
-
-root = tk.Tk()
-root.title("Keyboard Event Example")
-
-def readBytes():
-    
-    try:
-        sys.stdout.buffer.write(ser.read(293))
-    except serial.SerialException as e:
-        print("Error: Unable to open the serial port:", e)
-    except Exception as e:
-        print("An error occurred:", e)
-    #root.after(1, readBytes)
-
-# Bind the key press event to the root window
-root.bind("<KeyPress>", on_key_press)
-root.bind("<KeyRelease>", on_key_released)
-# Start the GUI event loop
-
-os.system('xset r off')
-root.after(1, readBytes)
-root.mainloop()
-
+# Print the ASCII art
+print(ascii_str)
